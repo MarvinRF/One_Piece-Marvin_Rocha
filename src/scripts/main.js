f1a88decf9fdde5ca56d7acec14efc80be8c8e02
MarvinRF <marvinvca@outlook.com>
2023-09-14 13:58:50 -0300

    media queries

---
+++ src/scripts/main.js
@@ -0,0 +1,47 @@
+function trocarImagem(){
+    var imagemDoHeader = document.getElementById("logo");
+    var larguraDaTela = window.innerWidth; // Obtém a largura da tela
+
+    if (larguraDaTela <= 768) {
+        // Se a largura da tela for menor ou igual a 768px, troque a imagem
+        imagemDoHeader.src = "dist/images/Netflix_N_logo.svg";
+    } else {
+        // Caso contrário, use a imagem padrão para desktop
+        imagemDoHeader.src = "dist/images/Netflix-Logo.wine.svg";
+    }
+}
+
+function trocarConteudo() {
+    var video = document.getElementById("video");
+    var imagem = document.getElementById("imagem");
+    var larguraDaTela = window.innerWidth; // Obtém a largura da tela
+
+    if (larguraDaTela <= 768) {
+        // Se a largura da tela for menor ou igual a 768px, oculta o vídeo e mostra a imagem
+        video.style.display = "none";
+        imagem.style.display = "block";
+    } else {
+        // Caso contrário, mostra o vídeo e oculta a imagem
+        video.style.display = "block";
+        imagem.style.display = "none";
+    }
+}
+
+function trocarbarra() {
+    var frase = document.getElementById("frase");
+    var botao = document.getElementById("botao");
+    var larguraDaTela = window.innerWidth; // Obtém a largura da tela
+
+    if (larguraDaTela <= 768) {
+        // Se a largura da tela for menor ou igual a 768px, injeta uma li a mais na barra e coloca display block
+        frase.style.display = "none";
+        botao.style.display = "block";
+    } else {
+        // Caso contrário, deixa como está
+        botao.style.display = "block";
+        frase.style.display = "none";
+    }
+}
+
+window.addEventListener("load", trocarImagem,trocarConteudo);
+window.addEventListener("resize", trocarImagem,trocarConteudo);
